b6ddcb9498e96053e88c200397838b30bf2707d0
RisheshG <142112687+RisheshG@users.noreply.github.com>
2025-03-02 18:22:24 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -8,8 +8,26 @@ const net = require("net");
 const cors = require("cors");
 const path = require("path");
 const axios = require("axios");
-const { admin, db } = require("./firebase"); // Import Firebase
+require("dotenv").config();
 
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 const app = express();
 const PORT = 5001;
